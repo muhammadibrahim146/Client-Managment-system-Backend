2bba9b447feada7d0a5c3b6e7c959b7ceace403f
Khalid <khalid@DESKTOP-O2LH4EU>
2026-09-15 01:30:06 +0500

    last changes hopfully

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: "https://client-managment-system-frontend-kt.vercel.app",
+    origin: "https://client-managment-system-frontend-my.vercel.app",
   })
 );
 
